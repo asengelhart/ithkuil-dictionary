de43452a32b006e90822acee981511e4817521ee
asengelhart <asengelhart@yahoo.com>
2020-01-18 01:12:30 -0700

    make_basic_root done, added some notes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require_relative './root.rb'
 require_relative './pattern.rb'
+require 'pry'
 
 class Scraper
   BASE_URL = 'http://www.ithkuil.net/lexicon.htm'
@@ -21,12 +22,27 @@ class Scraper
   end
 
   def self.make_basic_root(node)
-    rows = node.xpath('./tr')
+    rows = node.xpath('./tr | ./tbody/tr')
     definition_row = rows[0]
     phonetic_value = RootMatcher.match(definition_row.text)[0]
     translation = /‘.*’/.match(definition_row.text)[0]
     root = BasicRoot.new(phonetic_value, translation, get_notes(node))
-    make_patterns(root, node)
+    make_patterns(root, rows)
     @@dictionary << root
   end
+
+  def self.make_patterns(root, rows)
+    
+  end
 end
+
+# small.xpath('./tr').count == 7
+# small.xpath('./tr/td').count == 10
+# with_formal.xpath('./tr').count == 7
+# with_formal.xpath('./tr/td').count == 15
+# wtih_comp.xpath('./tr').count == 9
+# with_comp.xpath('./tr/td').count == 14
+# full.xpath('./tr').count == 9
+# full.xpath('./tr/td').count == 23
+
+all = doc.xpath('')
